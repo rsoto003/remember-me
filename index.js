1aa250522443bc53aa4cc3b53c7b2303c6d0bb99
Ryan Soto <rsoto003@ucr.edu>
2019-08-23 22:33:21 -0700

    added constant variable to keep track of eggs and omlettes on menu.

---
+++ index.js
@@ -0,0 +1,18 @@
+const eggsAndOmlettes = {
+    "1, 2, or 3 eggs": {
+        "ADD": "Bacon, Ham, or Sausage", 
+        "ADD": "Homemade Corned Beef Hash or Homemade Sausage"
+    },
+    "Cheese Omlette": {
+        "Contains": "Cheddar, Swiss, American or Muenster",
+        "ADD": "Bacon, Ham, Sausage or Chorizo"
+    },
+    "Meatlovers": "Bacon, sausage, ham, and cheddar",
+    "Denver": "Ham, green pepper and onions",
+    "Garbage": "4 Eggs, bacon, sausage, ham, 3 cheeses, veggies",
+    "Veggie": "mushroom, tomato, bell pepper, spinach, onion",
+    "Spinach, tomato, feta": "Spinach, tomato, feta",
+    "Irish": "Homemade corned beef hash and cheddar",
+    "Nado": "Swiss, tomato topped with avocado",
+    "Steak and Pepperjack": "red onions and roasted red peppers"
+}
